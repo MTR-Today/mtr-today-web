533e2edd4f275529e4c1047f66211ec83eeb8019
marklai1998 <marklai1998@gmail.com>
2022-10-12 00:10:16 +0800

    feat: bg

--- src/containers/Map/index.tsx
+++ src/containers/Map/index.tsx
@@ -38,8 +38,57 @@ export const Map = () => {
             }
             fill={colorMode === 'dark' ? '#FFFFFF' : '#000000'}
             opacity=".05"
-            stroke-linejoin="round"
-            strokeWidth="1px"
+          />
+          <path
+            d={
+              roundCorners(
+                'M 738 695 L 778 695 L 778 745 L 728 745 L 728 695 L 765 695',
+                10
+              ).path
+            }
+            fill={colorMode === 'dark' ? '#FFFFFF' : '#000000'}
+            opacity=".05"
+          />
+          <path
+            d={
+              roundCorners(
+                'M 390 950 L 480 950 L 695 735 L 760 800 L 760 1000 L 680 1000 L 480 1200 L 150 1200 L 150 950 L 400 950',
+                20
+              ).path
+            }
+            fill={colorMode === 'dark' ? '#FFFFFF' : '#000000'}
+            opacity=".05"
+          />
+          <path
+            d={
+              roundCorners(
+                'M 390 900 L 390 920 L 460 920 L 615 765 L 480 765 L 390 855 L 390 910',
+                20
+              ).path
+            }
+            fill={colorMode === 'dark' ? '#FFFFFF' : '#000000'}
+            opacity=".05"
+          />
+
+          <path
+            d={
+              roundCorners(
+                'M 830 1080 L 2650 1080 L 2650 1550 L 1350 1550 L 1160 1360 L 760 1360 L 760 1080 L 840 1080',
+                20
+              ).path
+            }
+            fill={colorMode === 'dark' ? '#FFFFFF' : '#000000'}
+            opacity=".05"
+          />
+          <path
+            d={
+              roundCorners(
+                'M 960 1400 L 1150 1400 L 1300 1550 L 900 1550 L 900 1400 L 1000 1400',
+                20
+              ).path
+            }
+            fill={colorMode === 'dark' ? '#FFFFFF' : '#000000'}
+            opacity=".05"
           />
           <Line
             d="M 250 500 L 250 240 L 400 240 L 400 530 L 1000 530 L 1000 750 L 1220 970 L 1265 1020 L 1520 1020 L 1665 876 L 1820 876 L 1950 750 L 1950 540 L 1596 540 L 1596 460 L 1820 460 L 1820 270 L 2430 270"
