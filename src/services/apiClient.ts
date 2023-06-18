990dbaf8d07b283e6f9a088d3aa8e665703e530f
marklai1998 <marklai1998@gmail.com>
2023-06-17 23:10:00 -0400

    feat: prod env

--- src/services/apiClient.ts
+++ src/services/apiClient.ts
@@ -1,6 +1,6 @@
 import wretch from 'wretch'
 import QueryStringAddon from 'wretch/addons/queryString'
 
-export const apiClient = wretch('http://dev-api.mtr.today:3000/api/v1').addon(
-  QueryStringAddon
-)
+export const apiClient = wretch(
+  `${import.meta.env.VITE_API_BASE_URL}/api/v1`
+).addon(QueryStringAddon)
