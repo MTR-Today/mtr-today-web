1867795cf45f5e560143a0eb3610280fbddf1e78
marklai1998 <marklai1998@gmail.com>
2023-06-23 23:40:55 -0400

    chore: remove global style

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,6 @@ import './locales'
 import './utils/dayjs'
 
 import { ChakraProvider, ColorModeScript, extendTheme } from '@chakra-ui/react'
-import { Global, css } from '@emotion/react'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { StrictMode } from 'react'
 import ReactDOM from 'react-dom/client'
@@ -31,15 +30,6 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
         <UseConfigProvider>
           <UseTimeProvider>
             <App />
-            <Global
-              styles={css`
-                html,
-                html * {
-                  // Prevent mobile scroll on Dnd
-                  touch-action: none;
-                }
-              `}
-            />
           </UseTimeProvider>
           <ColorModeScript initialColorMode={theme.config.initialColorMode} />
         </UseConfigProvider>
