32d4fe34f0bdc15f3569c80e05aebb0f4f899af0
marklai1998 <marklai1998@gmail.com>
2023-06-27 00:03:32 -0400

    refactor: use line and stop map

--- src/containers/StopDetail/index.tsx
+++ src/containers/StopDetail/index.tsx
@@ -12,6 +12,7 @@ import { StopCode, stopMap } from 'mtr-kit'
 import { useTranslation } from 'react-i18next'
 
 import { Language } from '../../constants/language'
+import { Empty } from '../StopSchedules/Empty'
 
 export const StopDetail: React.FC = () => {
   const navigate = useNavigate()
@@ -41,20 +42,26 @@ export const StopDetail: React.FC = () => {
         marginRight={{ md: '16px' }}
       >
         <DrawerCloseButton color={stop?.textColor} />
-        <DrawerHeader
-          textAlign="center"
-          bg={stop?.color}
-          color={stop?.textColor}
-          borderTopRadius="lg"
-          py="2"
-          transition="background-color 0.5s, color 0.5s"
-        >
-          {i18n.language === Language['ZH-HK'] ? stop?.nameZh : stop?.nameEn}
-          <Box fontSize="xs">{stop?.nameEn}</Box>
-        </DrawerHeader>
-        <DrawerBody p="0">
-          <Outlet />
-        </DrawerBody>
+        {stop ? (
+          <>
+            <DrawerHeader
+              textAlign="center"
+              bg={stop?.color}
+              color={stop?.textColor}
+              borderTopRadius="lg"
+              py="2"
+              transition="background-color 0.5s, color 0.5s"
+            >
+              {i18n.language === Language['ZH-HK'] ? stop.nameZh : stop.nameEn}
+              <Box fontSize="xs">{stop.nameEn}</Box>
+            </DrawerHeader>
+            <DrawerBody p="0">
+              <Outlet />
+            </DrawerBody>
+          </>
+        ) : (
+          <Empty />
+        )}
       </DrawerContent>
     </Drawer>
   )
