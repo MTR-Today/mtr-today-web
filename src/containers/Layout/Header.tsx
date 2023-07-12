a57c4516ef0738662efb7355c739bd32d312cd79
marklai1998 <marklai1998@gmail.com>
2023-07-11 21:47:47 -0400

    chore: remove type override

--- src/containers/Layout/Header.tsx
+++ src/containers/Layout/Header.tsx
@@ -95,31 +95,28 @@ export const Header: React.FC = () => {
       <HStack w="full" px="4">
         {menuMap(t).map(({ name, path }) => (
           <Link to={path} key={name}>
-            {
-              (({ isActive }: { isActive: boolean }) => (
-                <Button
-                  h="32px"
-                  px="4"
-                  fontSize="sm"
-                  lineHeight="32px"
-                  _hover={{ textDecoration: 'none' }}
-                  transition="background-color 1s"
-                  variant="link"
-                  {...(isActive
-                    ? {
-                        color: 'chakra-body-text',
-                        fontWeight: 'semibold',
-                        bg: colorMode === 'dark' ? 'whiteAlpha.50' : 'white',
-                        borderRadius: 'md',
-                        shadow: 'xs',
-                      }
-                    : {})}
-                >
-                  {name}
-                </Button>
-                // https://github.com/i18next/react-i18next/issues/1543
-              )) as unknown as React.ReactNode
-            }
+            {({ isActive }: { isActive: boolean }) => (
+              <Button
+                h="32px"
+                px="4"
+                fontSize="sm"
+                lineHeight="32px"
+                _hover={{ textDecoration: 'none' }}
+                transition="background-color 1s"
+                variant="link"
+                {...(isActive
+                  ? {
+                      color: 'chakra-body-text',
+                      fontWeight: 'semibold',
+                      bg: colorMode === 'dark' ? 'whiteAlpha.50' : 'white',
+                      borderRadius: 'md',
+                      shadow: 'xs',
+                    }
+                  : {})}
+              >
+                {name}
+              </Button>
+            )}
           </Link>
         ))}
       </HStack>
