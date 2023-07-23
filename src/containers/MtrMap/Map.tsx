a30547375606fb0476f9cf9b497350fbfa530a26
marklai1998 <marklai1998@gmail.com>
2023-07-22 23:42:57 -0400

    perf: scope line provider

--- src/containers/MtrMap/Map.tsx
+++ src/containers/MtrMap/Map.tsx
@@ -11,19 +11,19 @@ type Props = {
 }
 
 export const Map = memo(({ selectedLines = [] }: Props) => (
-  <lineContext.Provider value={{ selectedLines }}>
-    <svg
-      width={MAP_WIDTH}
-      height={MAP_HEIGHT}
-      style={{ position: 'absolute' }}
-      xmlns="http://www.w3.org/2000/svg"
-    >
-      <Island d="M 300 150 L 150 150 L 150 580 L 300 580 L 300 660 L 840 660 L 940 740 L 940 930 L 900 930 L 900 1035 L 1730 1035 L 2080 705 L 2200 705 L 2200 920 L 2300 1020 L 2650 1020 L 2650 600 L 2400 600 L 2400 400 L 2650 400 L 2650 150 L 1800 150 L 1800 400 L 1700 400 L 1700 200 L 900 200 L 750 350 L 600 350 L 400 150 L 280 150" />
-      <Island d="M 738 695 L 778 695 L 778 745 L 728 745 L 728 695 L 765 695" />
-      <Island d="M 390 950 L 480 950 L 695 735 L 760 800 L 760 1000 L 680 1000 L 480 1200 L 150 1200 L 150 950 L 400 950" />
-      <Island d="M 390 900 L 390 920 L 460 920 L 615 765 L 480 765 L 390 855 L 390 910" />
-      <Island d="M 830 1080 L 2650 1080 L 2650 1550 L 1350 1550 L 1160 1360 L 760 1360 L 760 1080 L 840 1080" />
-      <Island d="M 960 1400 L 1150 1400 L 1300 1550 L 900 1550 L 900 1400 L 1000 1400" />
+  <svg
+    width={MAP_WIDTH}
+    height={MAP_HEIGHT}
+    style={{ position: 'absolute' }}
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <Island d="M 300 150 L 150 150 L 150 580 L 300 580 L 300 660 L 840 660 L 940 740 L 940 930 L 900 930 L 900 1035 L 1730 1035 L 2080 705 L 2200 705 L 2200 920 L 2300 1020 L 2650 1020 L 2650 600 L 2400 600 L 2400 400 L 2650 400 L 2650 150 L 1800 150 L 1800 400 L 1700 400 L 1700 200 L 900 200 L 750 350 L 600 350 L 400 150 L 280 150" />
+    <Island d="M 738 695 L 778 695 L 778 745 L 728 745 L 728 695 L 765 695" />
+    <Island d="M 390 950 L 480 950 L 695 735 L 760 800 L 760 1000 L 680 1000 L 480 1200 L 150 1200 L 150 950 L 400 950" />
+    <Island d="M 390 900 L 390 920 L 460 920 L 615 765 L 480 765 L 390 855 L 390 910" />
+    <Island d="M 830 1080 L 2650 1080 L 2650 1550 L 1350 1550 L 1160 1360 L 760 1360 L 760 1080 L 840 1080" />
+    <Island d="M 960 1400 L 1150 1400 L 1300 1550 L 900 1550 L 900 1400 L 1000 1400" />
+    <lineContext.Provider value={{ selectedLines }}>
       {/* TML */}
       <Line
         d="M 250 500 L 250 240 L 400 240 L 400 530 L 1000 530 L 1000 750 L 1220 970 L 1265 1020 L 1520 1020 L 1665 876 L 1820 876 L 1950 750 L 1950 540 L 1596 540 L 1596 460 L 1820 460 L 1820 270 L 2430 270"
@@ -101,6 +101,6 @@ export const Map = memo(({ selectedLines = [] }: Props) => (
         flipTip
         startTip={false}
       />
-    </svg>
-  </lineContext.Provider>
+    </lineContext.Provider>
+  </svg>
 ))
